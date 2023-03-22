ba74ed693e0a921cc631de5b3606ac8ca9cfb3ea
Timur Ishmuratov <ishmuratov7timur7@gmail.com>
2023-03-22 23:10:17 +0000

    Added measurement function

--- include/QuantumCircuit.h
+++ include/QuantumCircuit.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <complex>
+#include <random>
 
 #include "Gates.h"
 #include "Matrix.h"
@@ -29,6 +30,40 @@ public:
                 m_state[j] = b;
             }
         }
+    }
+
+    int measure(int target_qubit) {
+        if (target_qubit < 0 || target_qubit >= m_num_qubits) {
+            std::cerr << "Error: Invalid target qubit" << std::endl;
+            return -1;
+        }
+
+        // Calculate the probability distribution for the target qubit
+        std::vector<double> probabilities(2);
+        int num_states = m_state.size();
+        for (int i = 0; i < num_states; i++) {
+            int bit = (i >> target_qubit) & 1;
+            probabilities[bit] += std::norm(m_state[i]);
+        }
+
+        // Randomly choose a measurement outcome based on the probability distribution
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<> dis(0, 1);
+        double r = dis(gen);
+        int outcome = (r < probabilities[0]) ? 0 : 1;
+
+        // Update the state of the quantum system based on the measurement outcome
+        for (int i = 0; i < num_states; i++) {
+            int bit = (i >> target_qubit) & 1;
+            if (bit != outcome) {
+                m_state[i] = 0;
+            } else {
+                m_state[i] /= sqrt(probabilities[outcome]);
+            }
+        }
+
+        return outcome;
     }
 
     friend std::ostream& operator<<(std::ostream& os, const QuantumCircuit& qc) {
