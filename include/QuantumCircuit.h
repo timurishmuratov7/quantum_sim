9dd5ee1e5e7e647e5becab5267d25885fc6a433d
Timur Ishmuratov <ishmuratov7timur7@gmail.com>
2023-03-22 22:57:05 +0000

    Apply operator added for Quantum circuit

--- include/QuantumCircuit.h
+++ include/QuantumCircuit.h
@@ -6,27 +6,25 @@
 #include <complex>
 
 #include "Gates.h"
+#include "Matrix.h"
 
 
 class QuantumCircuit {
 public:
     QuantumCircuit(int num_qubits) : m_num_qubits(num_qubits), m_state(1 << num_qubits) {}
 
-    void applyOperator(int target_qubit) {
+    void applyOperator(int target_qubit, Matrix<double, 2, 2> Operator) {
         if (target_qubit < 0 || target_qubit >= m_num_qubits) {
             std::cerr << "Error: Invalid target qubit" << std::endl;
             return;
         }
 
-        const std::complex<double> factor = 1.0 / sqrt(2.0);
-        const std::complex<double> H[2][2] = {{factor, factor}, {factor, -factor}};
-
         int num_states = m_state.size();
         for (int i = 0; i < num_states; i++) {
             if ((i >> target_qubit) & 1) {
                 int j = i ^ (1 << target_qubit);
-                std::complex<double> a = H[0][0] * m_state[i] + H[0][1] * m_state[j];
-                std::complex<double> b = H[1][0] * m_state[i] + H[1][1] * m_state[j];
+                double a = Operator.get(0, 0) * m_state[i] + Operator.get(0, 1) * m_state[j];
+                double b = Operator.get(1, 0) * m_state[i] + Operator.get(0, 1) * m_state[j];
                 m_state[i] = a;
                 m_state[j] = b;
             }
@@ -42,7 +40,7 @@ public:
 
 private:
     int m_num_qubits;
-    std::vector< std::complex<double> > m_state;
+    std::vector< double > m_state;
 };
 
 
